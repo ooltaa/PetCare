975d638d0fc311b42215b1d5a86eeb478432c3e3
ooltaa <oltalimani2@gmail.com>
2025-02-01 23:43:00 +0100

    edit

--- edit_request.php
+++ edit_request.php
@@ -24,7 +24,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Edit Request</title>
-    <link rel="stylesheet" href="CSS/request.css">
+    <link rel="stylesheet" href="CSS/edit.css">
 </head>
 <body>
     <div class="edit-request-container">
